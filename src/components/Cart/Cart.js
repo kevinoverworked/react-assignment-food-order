81f6c1f679e01fea1a253dab661a5be1f2619892
hallerk <kevin.haller@sincrodigital.com>
2021-12-01 09:27:50 -0800

    Moved the updateCartAmount to the Cart module to just tally up the items in the cart.

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useContext } from "react";
+import React, { Fragment, useContext, useState, useEffect } from "react";
 import CartItem from "./CartItem";
 import CartContext from "../../store/cart-context";
 import Button from "../UI/Button";
@@ -6,11 +6,28 @@ import classes from "./Cart.module.css";
 
 const Cart = () => {
     const cartCtx = useContext(CartContext);
+    const [cartTotalAmount, setCartTotalAmount] = useState(0);
+
+    useEffect(() => {
+        let cartTotal = 0;
+        
+        cartCtx.cartItems.forEach((item) => {
+            const total = parseInt(item.total, 10);
+            const price = item.price.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,");
+
+            cartTotal = cartTotal + (total * price);
+            console.log(cartTotal);
+        });
+        setCartTotalAmount(cartTotal);
+    }, [cartCtx]);
+
+
     return (
         <Fragment>
             <ul className={classes["cart-items"]}>
                 {cartCtx.cartItems.map((item) => (
                     <CartItem
+                        key={item.id}
                         name={item.name}
                         price={item.price}
                         amount={item.total}
@@ -19,7 +36,7 @@ const Cart = () => {
             </ul>
             <div className={classes.total}>
                 <span>Total Amount</span>
-                <span></span>
+                <span>${cartTotalAmount}</span>
             </div>
             <div className={classes.actions}>
                 <Button type="button" className={classes["button--alt"]} >Close</Button>
--- src/store/cart-context.js
+++ src/store/cart-context.js
@@ -8,14 +8,16 @@ const CartContext = React.createContext({
 
 export const CartContextProvider = (props) => {
     const [cartTotal, setCartTotal] = useState(0);
+    const [cartTotalAmount, setCartTotalAmount] = useState(0);
     const [cartItems, setCartItems] = useState([]);
 
     const updateCart = (data, amount) => {
         const cartIndex = cartItems.findIndex(item => item.id === data.id);
-
+        
         if (cartIndex < 0) {
             data["total"] = parseInt(amount, 10);
             return [...cartItems, data];
+
         } else {
             const newArray = [...cartItems];
             const newTotal = parseInt(newArray[cartIndex].total, 10) + parseInt(amount, 10);
@@ -24,10 +26,12 @@ export const CartContextProvider = (props) => {
                 ...newArray[cartIndex],
                 total: newTotal
             };
-
+            
             return newArray;
         }
     };
+
+    
 
     const addToCartHandler = (data, amount) => {
         const newTotal = parseInt(cartTotal, 10) + parseInt(amount, 10);
